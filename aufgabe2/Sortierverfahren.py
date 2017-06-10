3f65ced6af1d1d121121e7a06d574feab60da62b
schrist <SimonChrist@gmx.de>
2017-06-10 13:27:05 +0200

    copied max-heap from lecture and made it a min-heap

--- aufgabe2/Sortierverfahren.py
+++ aufgabe2/Sortierverfahren.py
@@ -10,6 +10,13 @@
 
 # Implementieren Sie ab hier Ihre Lösungen:
 
+# Vertauschen der Werte an Positionen i und j in Liste L
+# pre: 0 <= i, j <= len(L)-1
+def chg(L,i,j):
+    tmp= L[i]
+    L[i] = L[j]
+    L[j] = tmp
+
 def merge( list1, list2 ):
     length1 = len(list1)
     length2 = len(list2)
@@ -18,9 +25,7 @@ def merge( list1, list2 ):
     while j < length2:
         i = length1 + j
         while i > 0 and list[i-1] > list[i]:
-            tmp = list[i - 1]
-            list[i - 1] = list[i]
-            list[i] = tmp
+            chg( list, i-1, i )
             i = i - 1
         if i == length1 + j: break
         j = j + 1
@@ -65,10 +70,38 @@ def sortMerge(a):
     combine( result )
     return result
 
+def heapify(A,v,last):
+    w = 2 * v + 1 # linkes Kind von v
+    while(w <= last): # w ist Knoten des Baums
+        if(w+1 <= last): # Gibt es ein rechtes Kind?
+            if(A[w] > A[w+1]): w = w+1  # > statt < für min-heap
+        # w ist Kind mit minimalem Element
+        if(A[v] <= A[w]): return    # Heapeigenschaft o.k.; <= statt >= für min-heap
+        # sonst:
+        chg(A,v,w)
+        v = w
+        w = 2 * v + 1
+
+def buildHeap(A):
+    n = len(A) -1
+    # fueralle inneren Knoten bottom-up
+    for v in range( (n-1) // 2, -1, -1 ):
+        # Heapeigenschaft wieder herstellen
+        heapify(A,v,n)
+
 def sortHeapDesc(a):
     print('Parameter a:')
     print(a)
     # hier soll Ihre Implementierung von absteigendem HeapSort stehen.
+    buildHeap(a)
+    last = len(a) -1
+    while(last >= 1):
+        # Wurzel mit letztem Blatt tauschen
+        chg(a,last,0)
+        # letzten Knoten aus Baum entfernen
+        last = last -1
+        # Heapeigenschaft wieder herstellen
+        heapify(a,0,last)
     result = a  # durch das Ergebnis Ihrer Implementierung ersetzen
     return result
 
